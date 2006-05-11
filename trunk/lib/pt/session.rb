8d54ce9d2c30d6b3b0fcf465a8c648890960940e
jhardt <jhardt@8338a558-8d0c-11dd-8ab4-67102e0efeef>
2006-05-11 20:20:57 +0000

    Importing pt lib

---
+++ trunk/lib/pt/session.rb
@@ -0,0 +1,146 @@
+# This file is part of "qs".
+# 
+# "qs" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "qs" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "qs"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'csv'
+module PT
+  class Session
+    
+    attr_accessor :title
+    attr_reader :tracks
+    attr_accessor :print_frames
+    attr_reader :fps
+    attr_accessor :time_format
+    attr_accessor :blend
+    attr_accessor :shading
+    attr_accessor :cue_font_size
+    attr_accessor :proportional
+    attr_accessor :watermark
+    
+    def initialize
+      @title = "New Session"
+      @tracks = []
+      @print_frames = true
+      @fps = 24
+      @time_format = :footage
+      @blend = 1.0
+      @shading = :all # :none | :asterisks | :all
+      @cue_font_size = 10
+      @proportional = true
+      @watermark = nil
+    end
+    
+    def reframe!
+      @tracks.each {|t| t.reframe! }
+      @print_frames = false
+    end
+    
+    def interpret_tagging!
+      @tracks.each {|t| t.interpret_tagging! }
+    end
+
+    def add_track(name)
+      t = Track.new(self)
+      name != "" ? t.name = name : t.name = "A" + @tracks.size.succ.to_s
+      t.channel = @tracks.size.succ.to_s
+      @tracks << t
+      t
+    end
+
+    def read_file(io,line_ending = nil )
+      curr_tr, curr_region, reading = nil , nil , false
+
+      parse = proc do |line| 
+        line.split(%r/\t/).map{|c| c.strip}
+      end
+      
+      if line_ending then
+        my_line_ending = line_ending
+      else
+        my_line_ending = if io.gets("\n").size < 60 then
+            "\n"
+          elsif io.rewind && io.gets("\r\n").size < 60 then
+            "\r\n"
+          else
+            "\r"
+          end
+        io.rewind
+      end
+      
+      io.each(my_line_ending) do |line|
+      row = parse[line]
+      case row[0]
+        when 'SESSION NAME:'
+          self.title = row[1]
+          #$stderr.print "Reading name as #{row[1]}\n"
+        when 'TIME CODE FORMAT:'
+          @fps =  case row[1]
+                    when /29|30/
+                      @fps = 30
+                    when /25/
+                      @fps = 25
+                    when /24|23/
+                      @fps = 24
+                  end
+          #$stderr.print "Reading frame count as #{@fps}\n"        
+        when 'TRACK NAME:'
+          curr_tr = add_track(row[1])
+          #$stderr.print "Reading track named #{row[1]}\n"        
+        when 'CHANNEL'
+          reading = true if curr_tr && row[1] == 'EVENT' 
+          #$stderr.print "Channel detected for #{curr_tr.name}\n"
+        when '1'
+          if reading then
+            name = row[2].strip
+            name = "(blank)" unless name
+            r = curr_tr.add_region(name, row[3], row[4])
+            #$stderr.print "- Reading region #{name} at #{row[3]} - #{r.start_time}\n"
+          end
+        when nil
+          curr_str , reading = nil , false
+        end
+      end
+    end
+  
+    def display_tracks
+      @tracks
+    end
+
+    def display_regions
+      display_tracks.inject([]) {|all , track| all + track.regions}
+    end
+
+    def time_font_size
+      13
+    end
+
+    def finish_time_font_size
+      @cue_font_size
+    end
+  
+    def paper_format
+      #'LETTER' 
+      [ 0 , 0 , 792 , 1224 ] #tabloid
+    end
+  
+    def paper_orientation
+      :landscape
+    end
+  
+    def strips_per_page
+      16
+    end
+  end
+end #Module
---
+++ trunk/lib/pt/track.rb
@@ -0,0 +1,182 @@
+# This file is part of "qs".
+# 
+# "qs" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "qs" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "qs"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+module PT
+  class Track
+
+    attr_reader :session
+    attr_reader :regions
+    attr_accessor :name
+    attr_accessor :channel
+
+    def initialize(s)
+      @session = s
+      @regions = []
+      @name = "(blank)"
+      @channel = 0
+    end
+
+    def add_region(name = '(blank)' , start = 0, finish = 0)
+      r = Region.new(self)
+      r.name, r.start_time, r.finish_time = name , start , finish
+      @regions << r
+      r
+    end
+    
+    def add_primative_region(name,start,finish)
+      r = Region.new(self)
+      r.name, r.start, r.finish = name , start , finish
+      @regions << r
+      r
+    end
+
+    def reframe!
+      @regions.each {|r| r.reframe! }
+    end
+
+    def impose!(imposing_region)
+      overwritten = @regions.delete_if do |region|
+        region.start >= imposing_region.start && \
+        region.finish <= imposing_region.finish && \
+        region.object_id != imposing_region.object_id 
+      end
+      
+      trim_tail = @regions.find do |region|
+        region.finish > imposing_region.start && \
+        region.start < imposing_region.start
+      end
+      trim_tail.finish = imposing_region.start if trim_tail
+      
+      trim_head = @regions.find do |region|
+        region.start < imposing_region.finish && \
+        region.finish > imposing_region.finish
+      end
+      trim_head.start = imposing_region.finish if trim_head
+      
+    end
+    
+    def interpret_tagging!
+      legal_tags = [ "]" , "[" , "[[" , "]]" ,
+                     "}" , "{" , "{{" , "}}" ,
+                     ">" , "<" , "<<" , ">>" ,
+                     "&" , "!" , "!!" ]
+      
+      sequences = []
+      my_regions = @regions.dup
+      @regions = []
+      blend_duration = @session.blend * 600
+      last_o = 0 - blend_duration - 1
+    
+      my_regions.each do |region |
+        if region.start - last_o > blend_duration then
+          sequences << []
+        end
+        last_o = region.finish
+        (sequences.last).last.finish = region.start if sequences.last.last
+        sequences.last << region
+      end
+      
+      stick_open = false 
+      new_region = nil
+      
+      sequences.each do |seq|
+        unless stick_open then
+          seq_start = seq.first.start
+          new_region = nil 
+        end
+        
+        one_tagged = seq.inject(false) do |memo,region|
+          clean_name , tag = scan_region_name(region)
+          legal_tags.include? tag or memo
+        end
+        
+        seq.each do |region|
+          clean_name , tag = scan_region_name(region)
+          if legal_tags.include? tag then
+            curly_start = (stick_open ? region.start : seq_start)
+            case tag
+              when "]" , "["
+                new_region = add_primative_region(clean_name , region.start , region.finish)
+               
+              when "]]" , "[["
+                stick_open = true
+                new_region = add_primative_region(clean_name , region.start , region.finish)
+                
+              when "}" , "{"
+                new_region = add_primative_region(clean_name , curly_start ,region.finish)
+                 
+              when "}}" , "}}"
+                stick_open = true
+                new_region = add_primative_region(clean_name , curly_start ,region.finish)                
+              when ">" , "<"
+                add_primative_region("Fill" , seq_start , region.start) unless new_region
+                new_region = add_primative_region(clean_name , region.start , region.finish)
+                
+              when ">>" , "<<"
+                stick_open = true
+                add_primative_region("Fill" , seq_start , region.start) unless new_region
+                new_region = add_primative_region(clean_name , region.start , region.finish)
+                                
+              when "&"
+                if new_region then
+                  new_region.name = (new_region.name + " " + clean_name)
+                  new_region.finish = region.finish
+                else
+                  new_region = add_primative_region(clean_name , curly_start ,region.finish)                 
+                end
+              when "!"
+                if stick_open then
+                  stick_open = false
+                  new_region.finish = region.start
+                end
+                
+              when "!!"
+                if stick_open then
+                  stick_open = false
+                  new_region.finish = region.finish
+                end
+            end
+            seq_start = region.finish
+          else
+            if one_tagged then
+              if new_region then
+                new_region.finish = region.finish
+                seq_start = region.finish
+              else
+                
+              end
+            else
+              if new_region then
+                new_region.finish = region.finish
+              else
+                new_region = add_primative_region(clean_name,region.start,region.finish)
+              end
+            end
+          end
+        end
+      end # sequences.each       
+    end
+
+    def scan_region_name(region)
+#       md = /(.*)(-\]|-\[|-\}|-\{|-<|->|-\]\]|-\[\[|-\}\}|-\{\{|-<<|->>|-&|-!|-!!)$/.match(region.name)
+        md = /(.*)-([^-]*)$/.match(region.name)
+        return md[1] , md[2] if md
+        return region.name , nil
+    end
+    
+    
+  end
+end
