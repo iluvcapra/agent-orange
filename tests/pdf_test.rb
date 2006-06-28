589fcf3dffc898e9f88a22ade2dc5e49df7c645e
jhardt <jhardt@8338a558-8d0c-11dd-8ab4-67102e0efeef>
2006-06-28 05:17:10 +0000

    Moving branch_1 into trunk.

---
+++ tests/pdf_test.rb
@@ -0,0 +1,48 @@
+# pdf_test.rb
+# Author:: Jamie Hardt
+#
+# This file is part of "agent-orange".
+# 
+# "agent-orange" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "agent-orange" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "agent-orange"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'rubygems'
+require 'test/unit'
+$: << "lib/"
+require 'pt/session'
+require 'pt/track'
+require 'pt/region'
+require 'pdf_qs'
+
+# Tests simple track operations
+class PDFTest < Test::Unit::TestCase
+  
+  def setup
+    testfile = "tagging_test_cases/in/1sec_blend_test.txt"
+    
+    @session = PT::Session.new
+    File.open(testfile,"r") do |fp|
+      @session.read_file(fp)
+    end
+  end
+  
+  def test_to_pdf
+    qs = Cuesheet.new(@session)
+    assert_nothing_raised do
+      qs.to_pdf
+    end
+  end
+
+  
+end #class
