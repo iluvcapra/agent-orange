138f6929d057c73e4fa9b41bafc82b2d44482a40
jhardt <jhardt@8338a558-8d0c-11dd-8ab4-67102e0efeef>
2006-09-30 00:22:44 +0000

    Copied trunk into releases/1_2_0

---
+++ releases/1_2_0/tests/test_suite.rb
@@ -0,0 +1,29 @@
+# test_suite.rb
+# Author:: Jamie Hardt
+#
+# This file is part of "agent-orange".
+# 
+# "agent-orange" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "agent-orange" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "agent-orange"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+require 'test/unit'
+
+require 'tests/blend_test'
+require 'tests/track_test'
+require 'tests/tagging_tests'
+require 'tests/tagging_test_cases'
+require 'tests/pdf_test'
+require 'tests/session_test'
+require 'tests/styler_test'
