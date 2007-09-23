05df4c05742934ad14955530cfd9f48b75a33948
jhardt <jhardt@8338a558-8d0c-11dd-8ab4-67102e0efeef>
2007-09-23 07:48:33 +0000

    Adding OSX source

---
+++ trunk/osx/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Agent Orange
+//
+//  Created by Jamie Hardt on 4/9/06.
+//  Copyright __MyCompanyName__ 2006. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
